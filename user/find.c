cb078e6ac3cbb2b5bf7cd02a07a03c587466ca30
agree0002 <donghui1365@outlook.kr>
2026-07-18 21:48:39 +0900

    lab: utilities - find

---
+++ user/find.c
@@ -0,0 +1,77 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+#include "kernel/param.h"
+
+char *fargv[MAXARG];
+
+char *fname(char *path)
+{
+    char *p;
+	// Find first character after last slash.
+	for (p = path + strlen(path); p >= path && *p != '/'; p--)
+		;
+	return ++p;
+}
+
+void find(char *path, char *name)
+{
+	int fd;
+	struct stat st;
+
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		fprintf(2, "find: cannot open %s\n", path);
+		return;
+	}
+
+	if (fstat(fd, &st) < 0) {
+		fprintf(2, "find: cannot stat %s\n", path);
+		close(fd);
+		return;
+	}
+
+	char buf[512];
+	char *p;
+	struct dirent de;
+
+	switch (st.type) {
+	case T_DEVICE:
+	case T_FILE:
+		if (!strcmp(fname(path), name))
+			printf("%s\n", path);
+		break;
+
+	case T_DIR:
+		strcpy(buf, path);
+		p = buf + strlen(buf);
+		*p++ = '/';
+
+		while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+			if (de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
+				continue;
+
+			memmove(p, de.name, DIRSIZ);
+			p[DIRSIZ] = 0;
+
+			if (stat(buf, &st) < 0) {
+				printf("find: cannot stat %s\n", buf);
+				continue;
+			}
+
+			find(buf, name);
+		}
+    	break;
+	}
+	close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+		exit(0);
+ 
+    find(argv[1], argv[2]);
+	exit(0);
+}
